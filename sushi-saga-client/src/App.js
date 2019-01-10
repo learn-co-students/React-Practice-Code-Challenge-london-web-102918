edd793ab2e9191986f9eb1eb8588ab65bbea84bd
paolov1928 <ventura.paolo@gmail.com>
2019-01-10 15:07:39 +0000

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -5,16 +5,39 @@ import Table from './containers/Table';
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
+
+
+
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      sushi: {},
+      allSushis: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(r => r.json())
+      .then(json => this.setState({
+        allSushis: json
+      }))
+    }
+
+
+
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
+        <SushiContainer allSushis={this.state.allSushis} />
         <Table />
       </div>
     );
   }
 }
+
 
 export default App;
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,12 +1,20 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+// import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+
+  const getRandomSushi = () => {
+    return props.allSushis[Math.floor(Math.random() * props.allSushis.length)];
+  }
+  const sushi01 = getRandomSushi()
+  console.log(sushi01)
   return (
     <Fragment>
       <div className="belt">
+
         {
-          /* 
+          /*
              Render Sushi components here!
           */
         }
